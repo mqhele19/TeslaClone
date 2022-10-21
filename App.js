b5b1d66ec14db85580ab6a76f7611e3cc9c6c243
MqheleMugqakayiseMguni <mqhelemguni19@gmail.com>
2022-10-21 05:08:18 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,31 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import CarItem from './components/CarItem';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+    
+    <CarItem/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+//this is a single line comment
+/*
+this is
+a multiple
+line 
+comment 
+*/
+
+});
+
